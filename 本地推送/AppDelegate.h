a5f6b6824b55c1240bdebb9c3a56abd08b6e73ce
Miles <596442958@qq.com>
2016-03-01 16:04:54 +0800

    Initial Commit

---
+++ 本地推送/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  本地推送
+//
+//  Created by Miles on 3/1/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ 本地推送/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  本地推送
+//
+//  Created by Miles on 3/1/16.
+//  Copyright © 2016 Miles. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
